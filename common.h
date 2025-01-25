567950d2e7c20da7be662a2218d35a43c81d3ab1
Bennett Potgieter <potgieterbt@gmail.com>
2025-01-25 19:40:06 +0000

    started (upto printf)

---
+++ common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define va_list __builtin_va_list
+#define va_start __builtin_va_start
+#define va_end __builtin_va_end
+#define va_arg __builtin_va_arg
+
+struct sbiret {
+  long error;
+  long value;
+};
+
+void printf(const char *fmt, ...);
--- kernel.c
+++ kernel.c
@@ -1,1 +1,22 @@
+#include "kernel.h"
+#include "common.h"
 
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef uint32_t size_t;
+
+extern char __bss[], __bss_end[], __stack_top[];
+
+void kernel_main(void) {
+  printf("\n\nHello %s\n", "World!");
+  printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+  for (;;)
+    __asm__ __volatile__("wfi");
+}
+
+__attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
+  __asm__ __volatile__("mv sp, %[stack_top] \n"
+                       "j kernel_main\n"
+                       :
+                       : [stack_top] "r"(__stack_top));
+}
