c9dd43bf03ce899343897029eb3fa7c9d38f19ab
Bennett Potgieter <potgieterbt@gmail.com>
2025-01-26 17:21:57 +0000

    Upto processes and switching context

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # os_in_1000_lines_c
+
+Just a following of this [tutorial](https://operating-system-in-1000-lines.vercel.app/en/).
+
+I hope to add other features after finishing the tutorial.
--- common.c
+++ common.c
@@ -1,5 +1,54 @@
 #include "common.h"
 
+void *memcpy(void *dst, const void *src, size_t n) {
+  uint8_t *d = (uint8_t *)dst;
+  const uint8_t *s = (const uint8_t *)src;
+  while (n--) {
+    *d++ = *s++;
+  }
+  return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+  uint8_t *p = (uint8_t *)buf;
+  while (n--) {
+    *p++ = c;
+  }
+  return buf;
+}
+
+char *strcpy(char *dst, const char *src) {
+  char *d = dst;
+  while (*src) {
+    *d++ = *src++;
+  }
+  *d = '\0';
+  return dst;
+}
+
+char *strncpy(char *dst, const char *src, size_t n) {
+  char *d = dst;
+  while (n-- && *src) {
+    *d++ = *src++;
+  }
+  if (n != 0) {
+    // error
+  }
+  *d = '\0';
+  return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 && *s2) {
+    if (*s1 != *s2) {
+      break;
+    }
+    s1++;
+    s2++;
+  }
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
                        long arg5, long fid, long eid) {
   register long a0 __asm__("a0") = arg0;
