f892669900b2eefe393ad46c0b96370f2dd95204
Bennett Potgieter <potgieterbt@gmail.com>
2025-01-31 15:35:11 +0000

    File System(Tutorial Complete)

--- kernel.h
+++ kernel.h
@@ -27,6 +27,10 @@
 
 #define USER_BASE 0x1000000
 #define SSTATUS_SPIE (1 << 5)
+#define SSTATUS_SUM (1 << 18)
+
+#define FILES_MAX 2
+#define DISK_MAX_SIZE align_up(sizeof(struct file) * FILES_MAX, SECTOR_SIZE)
 
 #define SECTOR_SIZE 512
 #define VIRTQ_ENTRY_NUM 16
@@ -107,6 +111,34 @@ struct process {
   vaddr_t sp;
   uint32_t *page_table;
   uint8_t stack[8192];
+};
+
+struct tar_header {
+  char name[100];
+  char mode[8];
+  char uid[8];
+  char gid[8];
+  char size[12];
+  char mtime[12];
+  char checksum[8];
+  char type;
+  char linkname[100];
+  char magic[6];
+  char version[2];
+  char uname[32];
+  char gname[32];
+  char devmajor[8];
+  char devminor[8];
+  char prefix[155];
+  char padding[12];
+  char data[];
+} __attribute__((packed));
+
+struct file {
+  bool in_use;
+  char name[100];
+  char data[1024];
+  size_t size;
 };
 
 struct sbiret {
