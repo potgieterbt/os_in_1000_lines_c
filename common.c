2c7948b52d26c739a3431e69b593b08849d5ab61
Bennett Potgieter <potgieterbt@gmail.com>
2025-01-31 14:09:49 +0000

    Disk

--- common.c
+++ common.c
@@ -1,53 +1,40 @@
 #include "common.h"
 
+void *memset(void *buf, char c, size_t n) {
+  uint8_t *p = (uint8_t *)buf;
+  while (n--)
+    *p++ = c;
+  return buf;
+}
+
 void *memcpy(void *dst, const void *src, size_t n) {
   uint8_t *d = (uint8_t *)dst;
   const uint8_t *s = (const uint8_t *)src;
-  while (n--) {
+  while (n--)
     *d++ = *s++;
-  }
   return dst;
-}
-
-void *memset(void *buf, char c, size_t n) {
-  uint8_t *p = (uint8_t *)buf;
-  while (n--) {
-    *p++ = c;
-  }
-  return buf;
 }
 
 char *strcpy(char *dst, const char *src) {
   char *d = dst;
-  while (*src) {
+  while (*src)
     *d++ = *src++;
-  }
-  *d = '\0';
-  return dst;
-}
-
-char *strncpy(char *dst, const char *src, size_t n) {
-  char *d = dst;
-  while (n-- && *src) {
-    *d++ = *src++;
-  }
-  if (n != 0) {
-    // error
-  }
   *d = '\0';
   return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
   while (*s1 && *s2) {
-    if (*s1 != *s2) {
+    if (*s1 != *s2)
       break;
-    }
     s1++;
     s2++;
   }
+
   return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
+
+void putchar(char ch);
 
 void printf(const char *fmt, ...) {
   va_list vargs;
@@ -76,33 +63,35 @@ void printf(const char *fmt, ...) {
           putchar('-');
           value = -value;
         }
+
         int divisor = 1;
-        while (value / divisor > 9) {
+        while (value / divisor > 9)
           divisor *= 10;
-        }
+
         while (divisor > 0) {
           putchar('0' + value / divisor);
           value %= divisor;
           divisor /= 10;
         }
+
         break;
       }
-
       case 'x': {
         int value = va_arg(vargs, int);
         for (int i = 7; i >= 0; i--) {
-          int nibble = (value >> (i * 4)) & 0xF;
+          int nibble = (value >> (i * 4)) & 0xf;
           putchar("0123456789abcdef"[nibble]);
         }
-        break;
       }
       }
     } else {
       putchar(*fmt);
     }
+
     fmt++;
   }
 
 end:
   va_end(vargs);
 }
+
